75069fa7be20cdfa2130c1742d539fc00606e5fc
Sabo <josepcoronado1@gmail.com>
2022-05-06 12:57:23 +0200

    submit exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3 || (process.argv.length > 3 && process.argv.length < 5)) {
+    console.log('Error: wrong arguments')
+    console.log('* Contact list: node mongo.js <password>')
+    console.log('* Add new contact: node mongo.js <password> <name> <number>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const uri = `mongodb+srv://josep:${password}@cluster0.fi6f1.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(uri)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+        process.exit(0)
+    })
+}
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
